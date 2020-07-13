da6064bc92074539ecb47589ecacd87e8bb6d4dd
C.A.P. Linssen <charl@turingbirds.com>
2020-07-13 16:53:53 +0200

    add jupyter notebook config file

---
+++ materials/docker/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+# Configuration file for jupyter-notebook.
+
+c.NotebookApp.password = 'sha1:7a12c3c3439a:d74662fead39f17cabd747faf34a65bd58d135d3'
+c.NotebookApp.password_required = True
+
+c.NotebookApp.port = 7002
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
