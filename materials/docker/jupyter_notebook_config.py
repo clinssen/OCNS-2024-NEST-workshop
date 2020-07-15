cdc3b960e8abb5dda8fc6fe42c59c1d3fa40e712
C.A.P. Linssen <charl@turingbirds.com>
2020-07-15 20:49:22 +0200

    update Jupyter notebook IP address

--- materials/docker/jupyter_notebook_config.py
+++ materials/docker/jupyter_notebook_config.py
@@ -4,5 +4,5 @@ c.NotebookApp.password = 'sha1:7a12c3c3439a:d74662fead39f17cabd747faf34a65bd58d1
 c.NotebookApp.password_required = True
 
 c.NotebookApp.port = 7003
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
