a64ea134967aa60beca0524b7cfbf3aa73bb6518
C.A.P. Linssen <charl@turingbirds.com>
2021-02-02 12:37:21 +0100

    use latest NESTML master instead of 3.1 release

--- docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
+++ docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
@@ -5,7 +5,7 @@ source /opt/nest/bin/nest_vars.sh
 
 
 cd /tmp
-wget https://raw.githubusercontent.com/clinssen/OCNS-2020-workshop/master/materials/docker/jupyter_notebook_config.py
+wget https://raw.githubusercontent.com/clinssen/OCNS-2020-workshop/master/materials/docker/nest-nestml-tutorial/jupyter_notebook_config.py
 mkdir $HOME/.jupyter
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
 cd
--- docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
+++ docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 # Configuration file for jupyter-notebook.
 
-c.NotebookApp.password = 'sha1:7a12c3c3439a:d74662fead39f17cabd747faf34a65bd58d135d3'
+c.NotebookApp.password = 'sha1:7a12c3c3439a:d74662fead39f17cabd747faf34a65bd58d135d3' # the password is: hellocns2020
 c.NotebookApp.password_required = True
 
 c.NotebookApp.port = 7003
