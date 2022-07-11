9cd1d5bcf9d27233cfd63fa857ec69c427d8fed3
clinssen <c.linssen@fz-juelich.de>
2022-07-11 16:04:13 +0200

    Update entrypoint.sh

--- docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
+++ docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
@@ -8,6 +8,7 @@ cd /tmp
 wget https://raw.githubusercontent.com/clinssen/OCNS-2022-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
 mkdir $HOME/.jupyter
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
+cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_lab_config.py
 cd
 
 
