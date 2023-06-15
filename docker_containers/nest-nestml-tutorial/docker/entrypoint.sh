deece94b343f70f189e11bfa4b4d02b09df9823c
C.A.P. Linssen <charl@turingbirds.com>
2023-06-15 04:17:06 -0700

    update for new NEST Simulator and NESTML version

--- docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
+++ docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
@@ -5,7 +5,7 @@ source /opt/nest/bin/nest_vars.sh
 
 
 cd /tmp
-wget https://raw.githubusercontent.com/clinssen/OCNS-2022-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
+wget https://raw.githubusercontent.com/clinssen/OCNS-2023-NEST-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
 mkdir $HOME/.jupyter
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_lab_config.py
@@ -15,11 +15,12 @@ cd
 
 jupyter-lab &
 
-mv -f OCNS-2022-workshop /tmp/
-git clone https://github.com/clinssen/OCNS-2022-workshop
-cd OCNS-2022-workshop/materials/nestml
+mv -f OCNS-2023-NEST-workshop /tmp/
+git clone https://github.com/clinssen/OCNS-2023-NEST-workshop
+cd OCNS-2023-NEST-workshop/materials
+mv nestml /tmp/orig-workshop-NESTML-materials
 git clone https://github.com/nest/nestml
-cd ../../..
+cd ..
 
 /bin/bash
 
