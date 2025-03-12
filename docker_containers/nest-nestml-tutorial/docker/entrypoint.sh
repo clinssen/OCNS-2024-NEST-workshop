9049d490c1e264d4e47949acdf6dbe660017f389
C.A.P. Linssen <charl@turingbirds.com>
2025-03-12 12:09:19 +0100

    update Ubuntu version

--- docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
+++ docker_containers/nest-nestml-tutorial/docker/entrypoint.sh
@@ -5,23 +5,23 @@ source /opt/nest/bin/nest_vars.sh
 
 
 cd /tmp
-wget https://raw.githubusercontent.com/clinssen/OCNS-2023-NEST-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
+wget https://raw.githubusercontent.com/clinssen/OCNS-2024-NEST-workshop/master/docker_containers/nest-nestml-tutorial/jupyter_notebook_config.py
 mkdir $HOME/.jupyter
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_notebook_config.py
 cp jupyter_notebook_config.py $HOME/.jupyter/jupyter_lab_config.py
 cd
 
+git clone https://github.com/nest/nestml
 
 
 jupyter-lab &
 
-mv -f OCNS-2023-NEST-workshop /tmp/
-git clone https://github.com/clinssen/OCNS-2023-NEST-workshop
-git clone https://github.com/nest/nestml
-cd OCNS-2023-NEST-workshop/materials
-mv nestml /tmp/orig-workshop-NESTML-materials
-ln -s ../../nestml/doc/tutorials nestml
-cd ..
+#mv -f OCNS-2024-NEST-workshop /tmp/
+#git clone https://github.com/clinssen/OCNS-2024-NEST-workshop
+#cd OCNS-2024-NEST-workshop/materials
+#mv nestml /tmp/orig-workshop-NESTML-materials
+#ln -s ../../nestml/doc/tutorials nestml
+#cd ..
 
 /bin/bash
 
